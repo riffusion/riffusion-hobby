7325bfa1439d5cd743cdea3f532cad59769e05c0
Hayk Martiros <hayk.mart@gmail.com>
2023-01-06 01:59:19 +0000

    Linter fixes
    Topic: audio_to_audio

--- riffusion/streamlit/util.py
+++ riffusion/streamlit/util.py
@@ -167,8 +167,10 @@ def select_device(container: T.Any = st.sidebar) -> str:
 
     device_options = ["cuda", "cpu", "mps"]
     device = st.sidebar.selectbox(
-        "Device", options=device_options, index=device_options.index(default_device),
-        help="Which compute device to use. CUDA is recommended."
+        "Device",
+        options=device_options,
+        index=device_options.index(default_device),
+        help="Which compute device to use. CUDA is recommended.",
     )
     assert device is not None
 
